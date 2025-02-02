1a0a8416701ed654bbb73170407f77ca7f631bcd
StanleyElain <ekirustanley17@gmail.com>
2025-02-02 21:07:19 +0300

    Add files via upload

---
+++ Timed_Math.py
@@ -0,0 +1,45 @@
+# Generating random math questions and timing how long it takes users to answer them
+
+import random
+import time
+
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEM = 10
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    
+    # Using eval function to evaluate expressions
+
+    answer = eval(expr)
+    return expr, answer
+
+
+
+wrong = 0
+input("Press enter to start!")
+print("-----------------------")
+
+# Implementing a timer function 
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEM):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("-----------------------")
+print("Nice Work! You finished in ", total_time, "seconds!")
